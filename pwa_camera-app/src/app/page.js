1f09b1441729e74dee88ee9cd190b233751005cc
Gabriel Hansmann <gabrielhansmann@web.de>
2024-07-19 18:06:40 +0200

    [Refactor] adding test buttons to page

--- pwa_camera-app/src/app/page.js
+++ pwa_camera-app/src/app/page.js
@@ -1,10 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
-    <p>
-      Hello World
-    </p>
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
   )
 }
