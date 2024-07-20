bdadfc532b3b3faa1767a6253f5c0a8261792916
Gabriel Hansmann <gabrielhansmann@web.de>
2024-07-20 21:17:02 +0200

    [ADD] psa compatibility

---
+++ pwa_camera-app/src/app/head.jsx
@@ -0,0 +1,22 @@
+export default function Head() {
+    return (
+      <>
+        <title>PWA Camera App</title>
+        <meta name="description" content="An example PWA Camera App" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/public/icon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/public/icon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/public/icon/favicon-16x16.png" />
+        <link rel="manifest" href="/public/icon/site.webmanifest" />
+        <link rel="mask-icon" href="/public/icon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/public/icon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#00aba9" />
+        <meta name="msapplication-config" content="/public/icon/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </>
+    );
+  }
+  
